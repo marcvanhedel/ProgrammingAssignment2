837ff4ee8d4503cf5e2613d10efddf05ef0440e1
unknown <marc.vanhedel@LPT-Daniel.custintgroup.lan>
2014-12-18 13:40:56 +0100

    v.1.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## =================================================================
+## =================================================================
+## = These functions are used to calculate the inverse of a matrix x
+## = and cache the result.
+## = If the inverse matrix has already been calculated, 
+## = it is returned without the need to recalculate.
+## = First call makeCacheMatrix(x) with x as matrix
+## = Then call cacheSolve(x)
+## =================================================================
+## =================================================================
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## =================================================================
+## = makeCacheMatrix (x = matrix())
+## = function to store and retrieve a matrix and its inverse
+## = functions
+## =    get() - gets the original matrix
+## =    set(x) - stores a matrix
+## =    getInvMatrix() - retrieves the cached inverse matrix
+## =    setInvMatrix(x) - stores inverse matrix in m
+## =================================================================
+makeCacheMatrix <- function(x=matrix()) {
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    
+    setInvMatrix <- function(invMatrix) m <<- invMatrix
+    
+    getInvMatrix <- function() m
+    
+    ## return a list of functions as an R object
+    list (set=set, 
+          get=get, 
+          setInvMatrix=setInvMatrix, 
+          getInvMatrix=getInvMatrix)
 }
 
+## =================================================================
+## = cacheSolve(x)
+## = Function to solve the matrix. (calculate inverse)
+## = If the matrix already has been solved, no need to recalculate
+## = Otherwise solve the matrix and store it
+## =================================================================
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    m <- x$getInvMatrix()
+    if(!is.null(m)){
+        ## Ha! No need to recalculate it        
+        message("Getting cached data... No need to recalculate...")
+        return(m)
+    }
+    else {
+        message("We need to solve this matrix... Calculating...")
+        data <- x$get()
+        m <- solve(data)
+        x$setInvMatrix(m)
+        message("Got it!")
+        return(m)
+    }
 }
